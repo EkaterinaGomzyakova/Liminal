267a772b16a7a43bfe4061637b521f32170b60e5
EkaterinaGomzyakova <68771341+EkaterinaGomzyakova@users.noreply.github.com>
2022-07-10 17:21:03 +0300

    Ver1

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 let phrases = [
-  { text: 'отправить другу смешную гифку', image: 'https://code.s3.yandex.net/web-code/procrastinate/1.gif' },
-  { text: 'посмотреть скидки на авиабилеты', image: 'https://code.s3.yandex.net/web-code/procrastinate/2.png' },
-  { text: 'разобраться, о чём поют рэперы', image: 'https://code.s3.yandex.net/web-code/procrastinate/3.png' },
-  { text: 'Юрий Дудь', image: 'https://code.s3.yandex.net/web-code/procrastinate/4.png' },
-  { text: 'расставить книги на полке по цвету', image: 'https://code.s3.yandex.net/web-code/procrastinate/5.png' },
-  { text: 'читать про зарплаты в Сан-Франциско', image: 'https://code.s3.yandex.net/web-code/procrastinate/6.png' },
-  { text: 'прочитать новости и ужаснуться в комментариях', image: 'https://code.s3.yandex.net/web-code/procrastinate/7.png' },
-  { text: 'попасть в поток грустных песен и вспомнить все ошибки молодости', image: 'https://code.s3.yandex.net/web-code/procrastinate/8.png' },
-  { text: 'посмотреть трейлер сериала и заодно первый сезон', image: 'https://code.s3.yandex.net/web-code/procrastinate/9.png' },
-  { text: 'проверить непрочитанное в Telegram-каналах', image: 'https://code.s3.yandex.net/web-code/procrastinate/10.png' }
+  { text: 'Генератор Аркадьева-Маркса', image: 'generator.jpg' },
+  { text: 'Горбовская ГЭС', image: 'gorgas.jpg' },
+  { text: 'Пионерский лагерь «Сказка»', image: 'camp.jpg' },
+  { text: 'Планетарий в санатории «Солнечное»', image: 'planetarium.jpg' },
+  { text: 'Ткварчели, Абхазия', image: 'town.jpg' },
+  { text: 'ЗИЛ', image: 'zil.jpg' },
+  { text: 'Хэлсиен Холл', image: 'halsien.jpg' },
+  { text: 'Стaнция Пcырцхa, Абхaзия', image: 'psyrckha.jpg' },
+  { text: 'Башня-ресторан, Грузия, Анаклия.', image: 'anaklia.jpg' },
+  { text: 'Усадьба Демидовых', image: 'demidov.jpg' }
 ];
 
 function getRandomElement(arr) {
